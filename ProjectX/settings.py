5986eef5780e2fae7a493413c48aaccd338a3824
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-03-05 18:53:07 -0800

    static root fixes

--- ProjectX/settings.py
+++ ProjectX/settings.py
@@ -122,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
