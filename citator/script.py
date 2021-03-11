c903b4ed3c4b6af65b9ef3756062f31d08115b23
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-03-10 21:40:43 -0800

    changed functionality

--- citator/script.py
+++ citator/script.py
@@ -1,2 +1,11 @@
 def GetResult(n):
-    return n*5
+    return n.title()
+def GetWordCount(n):
+    return len(n.split())
+def twoSum(arr,target):
+    hm = {}
+    for i,n in enumerate(arr):
+        if n in hm:
+            return [hm[n], i]
+        else:
+            hm[target-n] = i
--- citator/views.py
+++ citator/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 from citator.forms import CitatorForm
-from citator.script import GetResult
+from citator.script import GetResult,GetWordCount
 def CitatorView(request):
     return render(request,'citator.html')
 
 def CitatorPost(request):
     try:
-        content = int(request.POST.get('content',0))
-    except ValueError:
+        content = request.POST.get('content',0)
+        # content = list(map(int, content))
+    except:
         return HttpResponse("Integer Value Needed")
-    args = {"content": GetResult(content)}
+    args = {"content": GetResult(content), "count":GetWordCount(content)}
     return render(request,'citator.html',args)
