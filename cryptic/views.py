aac3f5ef10bd74ad1cd9b96dceb60033eb790599
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-03-11 20:46:23 -0800

    add dropdown

--- cryptic/views.py
+++ cryptic/views.py
@@ -8,18 +8,28 @@ def CrypticView(request):
     return render(request,'cryptic.html')
 
 def CrypticPost(request):
-    try:
-        content = request.POST.get('content',0)
-        key = int(request.POST.get('key',0)) % 26
+    content = request.POST.get('content',0)
+    cipher_type = request.POST.get("cipher_type")
+    crypt_type = request.POST.get('method_type',"encrypt")
+
+    if cipher_type == "caesar":
+        try:
+            key = int(request.POST.get('key',0)) % 26
+        except:
+            return HttpResponse("Error")
         if key<0:
             key = 26-abs(key)
-        crypt_type = request.POST.get('type',"encrypt")
-        # content = list(map(int, content))
-    except:
-        return HttpResponse("Error")
-    if crypt_type=="encrypt":
-        val = caesar(content,key)
-    elif crypt_type=="decrypt":
-        val = caesar(content,-key)
+        if crypt_type=="encrypt":
+            val = caesar(content,key)
+        elif crypt_type=="decrypt":
+            val = caesar(content,-key)
+
+    elif cipher_type == "vigenere":
+        key = request.POST.get('key')
+        if crypt_type == "encrypt":
+            val = vigenere_encrypt(content,key)
+        elif crypt_type == "decrypt":
+            val = vigenere_decrypt(content,key)
+            
     args = {"content": val}
     return render(request,'cryptic.html',args)
