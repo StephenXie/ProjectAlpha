bdba6b64dd71872e15f621a920c4ccb15f67bfc2
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-03-11 19:44:07 -0800

    bug fix

--- cryptic/script.py
+++ cryptic/script.py
@@ -2,13 +2,16 @@ def caesar_encrypt(plain_text, key):
     encrypted = list(plain_text)
     for i in range(len(encrypted)):
         if encrypted[i].isalpha():
-            cur_val = ord(encrypted[i])
-            encrypted[i] = chr((((cur_val-ord("a"))+key)%26)+ord("a"))
+            encrypted[i] = get_letter_val(encrypted[i],key)
     return "".join(encrypted)
+def get_letter_val(letter, key):
+    if letter.isupper():
+        return chr((((ord(letter)-ord("A"))+key)%26)+ord("A"))
+    else:
+        return chr((((ord(letter)-ord("a"))+key)%26)+ord("a"))
 def caesar_decrypt(encrypted, key):
     plain_text = list(encrypted)
     for i in range(len(plain_text)):
         if plain_text[i].isalpha():
-            cur_val = ord(encrypted[i])
-            plain_text[i] = chr((((cur_val-ord("a"))-key)%26)+ord("a"))
+            plain_text[i] = get_letter_val(encrypted[i],-key)
     return "".join(plain_text)
