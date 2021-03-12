a21538bada2f5dde0ca385d41470be8c4fb9a8cc
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-03-12 11:15:27 -0800

    Revert "Revert "add vigenere""
    This reverts commit dd9d777bfcc1e318a2bc035a4b958018ecb5c176.

--- cryptic/script.py
+++ cryptic/script.py
@@ -12,7 +12,22 @@ def ceasar_modify(letter, key):
         return chr((((ord(letter)-ord("a"))+key)%26)+ord("a"))
 
 def vigenere_encrypt(text, keyword):
-    return
+    res = list(text)
+    j = 0
+    for i in range(len(res)):
+        if res[i].isalpha():
+            val = ((ord(res[i]) - ord("a")) + (ord(keyword[j%len(keyword)]) - ord("a")))%26
+            res[i] = chr(val+ord("a"))
+            j+=1
+    return "".join(res)
 
 def vigenere_decrypt(text, keyword):
-    return
+    res = list(text)
+    j = 0
+    for i in range(len(res)):
+        if res[i].isalpha():
+            print(res)
+            val = (26 + (ord(res[i]) - ord("a")) - (ord(keyword[j%len(keyword)]) - ord("a")))%26
+            res[i] = chr(val+ord("a"))
+            j+=1
+    return "".join(res)
