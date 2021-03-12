f840265593933fbf40ac6f104718b059c5f7eefe
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-03-11 22:16:13 -0800

    bug fix

--- cryptic/views.py
+++ cryptic/views.py
@@ -26,10 +26,11 @@ def CrypticPost(request):
 
     elif cipher_type == "vigenere":
         key = request.POST.get('key')
+        content = content.lower()
         if crypt_type == "encrypt":
             val = vigenere_encrypt(content,key)
         elif crypt_type == "decrypt":
             val = vigenere_decrypt(content,key)
-            
+
     args = {"content": val}
     return render(request,'cryptic.html',args)
