3d25deea83516091aec059856e1ff5ea4ad30330
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-03-11 20:08:26 -0800

    minor changes

--- cryptic/script.py
+++ cryptic/script.py
@@ -1,17 +1,18 @@
-def caesar_encrypt(plain_text, key):
-    encrypted = list(plain_text)
-    for i in range(len(encrypted)):
-        if encrypted[i].isalpha():
-            encrypted[i] = get_letter_val(encrypted[i],key)
-    return "".join(encrypted)
-def get_letter_val(letter, key):
+def caesar(text, key):
+    res = list(text)
+    for i in range(len(res)):
+        if res[i].isalpha():
+            res[i] = ceasar_modify(res[i],key)
+    return "".join(res)
+
+def ceasar_modify(letter, key):
     if letter.isupper():
         return chr((((ord(letter)-ord("A"))+key)%26)+ord("A"))
     else:
         return chr((((ord(letter)-ord("a"))+key)%26)+ord("a"))
-def caesar_decrypt(encrypted, key):
-    plain_text = list(encrypted)
-    for i in range(len(plain_text)):
-        if plain_text[i].isalpha():
-            plain_text[i] = get_letter_val(encrypted[i],-key)
-    return "".join(plain_text)
+
+def vigenere_encrypt(text, keyword):
+    return
+
+def vigenere_decrypt(text, keyword):
+    return
--- cryptic/views.py
+++ cryptic/views.py
@@ -3,21 +3,23 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 # from citator.forms import CitatorForm
 # from citator.script import GetResult,GetWordCount, GetCharCount
-from cryptic.script import caesar_decrypt,caesar_encrypt
+from cryptic.script import caesar, vigenere_encrypt, vigenere_decrypt
 def CrypticView(request):
     return render(request,'cryptic.html')
 
 def CrypticPost(request):
     try:
         content = request.POST.get('content',0)
-        key = abs(int(request.POST.get('key',0)))
+        key = int(request.POST.get('key',0)) % 26
+        if key<0:
+            key = 26-abs(key)
         crypt_type = request.POST.get('type',"encrypt")
         # content = list(map(int, content))
     except:
         return HttpResponse("Error")
     if crypt_type=="encrypt":
-        val = caesar_encrypt(content,key)
+        val = caesar(content,key)
     elif crypt_type=="decrypt":
-        val = caesar_decrypt(content,key)
+        val = caesar(content,-key)
     args = {"content": val}
     return render(request,'cryptic.html',args)
