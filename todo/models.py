b6c6a0606f2d8aea77bf7e08ed615d8d59f3f38d
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-03-19 21:01:04 -0700

    change timezone

--- todo/models.py
+++ todo/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 from datetime import datetime
 import pytz
-def getDate():
-    tz = pytz.timezone('US/Pacific')
-    d = datetime.now(tz)
-    return d
 # Create your models here.
 class TodoItem(models.Model):
     content = models.TextField()
-    date = models.DateTimeField(default=getDate)
+    date = models.DateTimeField(default=datetime.now)
--- todo/views.py
+++ todo/views.py
@@ -2,14 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import TodoItem
 from django.forms.models import model_to_dict
-
+from django.utils.timezone import localtime
 # Create your views here.
+class item:
+    def __init__(self,date,content,id_):
+        self.date = localtime(date)
+        self.content = content
+        self.id = id_
 def todoView(request):
     all_todo_items = TodoItem.objects.all()
+    items = []
     for i in all_todo_items:
-        print(model_to_dict(i)['date'])
+        items.append(item(i.date,i.content,i.id))
+
     return render(request, 'todo.html',
-                  {'all_items': all_todo_items})
+                  {'all_items': all_todo_items,'items':items})
 
 
 def addTodo(request):
