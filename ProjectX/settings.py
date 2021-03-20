70ec1fc1b0dcf07447c2d4e323d45a00f4be7127
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-03-20 11:45:46 -0700

    add delete button for gpacal

--- ProjectX/settings.py
+++ ProjectX/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 ROOT_URLCONF = 'ProjectX.urls'
 
 TEMPLATES = [
