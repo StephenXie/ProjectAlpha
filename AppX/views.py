8e5db770449b51c323de88e6a20a4642bc5dafbb
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-03-22 22:44:16 -0700

    add 404 page; Change page titles

--- AppX/views.py
+++ AppX/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
 # Create your views here.
 def hi(request):
     return render(request,'AppX/hi.html')
+
+def handler404(request, *args, **argv):
+    response = render(request,'404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
