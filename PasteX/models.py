2e140cc6693eaa0ab23f629e049e1857757fd18d
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-04-09 12:24:19 -0700

    PasteX add syntax highlighting

--- PasteX/models.py
+++ PasteX/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class PasteXItem(models.Model):
     id = models.CharField(max_length=30,primary_key=True)
     content = models.TextField()
+    language = models.TextField()
--- PasteX/views.py
+++ PasteX/views.py
@@ -12,12 +12,13 @@ def PasteXView(request):
             item = PasteXItem.objects.get(pk=my_id)
         except ObjectDoesNotExist:
             return render(request,'404.html')
-        return render(request,"pastex.html",{'item':item})
+        return render(request,"pastex.html",{'item':item,"language":item.language})
     elif request.method == "POST":
         # add item to model and return id
         content = request.POST.get('content')
         new_id = b64(hashlib.sha256(bytes(content,'utf8')).hexdigest()).replace("=","")[:21]
-        new_item = PasteXItem(content=content,id=new_id)
+        language = request.POST.get('content_type')
+        new_item = PasteXItem(content=content,id=new_id,language=language)
         new_item.save()
         url = request.build_absolute_uri
         return render(request,"pastex.html",{'item_id':new_id})
