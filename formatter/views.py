7967b3d1f2229a8c31e3c823bf114a74579249fe
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-06-09 15:26:09 -0700

    Add redirection for formatter

--- formatter/views.py
+++ formatter/views.py
@@ -6,6 +6,8 @@ from formatter.scripts import GetResult, GetWordCount, GetCharCount
 
 
 def FormatterView(request):
+    if request.path == "/formatter/post":
+        return HttpResponseRedirect("/formatter")
     if request.method == "POST":
         try:
             content = request.POST.get('content', 0)
