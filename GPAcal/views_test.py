63ef3b5a30553e3df35d4578827242a8a8c08e1d
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-09-06 14:32:14 -0700

    Add raw gpa to api

--- GPAcal/views_test.py
+++ GPAcal/views_test.py
@@ -13,7 +13,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 def api_GPAcal(request):
-    GPA_u = GPA_w = max_GPA = 4.0
+    raw_GPA_u = raw_GPA_w = max_GPA = 4.0
     if request.method == "POST":
         print("SX")
         data = json.loads(request.body.decode("utf-8"))
@@ -22,15 +22,15 @@ def api_GPAcal(request):
         grades = data.get("grade")
         class_type = data.get("class_type")
         credit = data.get("credit")
-        GPA_w = getGPA_w(grades, class_type, credit)
-        GPA_u = getGPA_u(grades, credit)
+        raw_GPA_w = getGPA_w(grades, class_type, credit)
+        raw_GPA_u = getGPA_u(grades, credit)
         max_GPA = getMaxGPA(class_type)
-    pct_u = (GPA_u/4)*100
+    pct_u = (raw_GPA_u/4)*100
     pct_u = round(pct_u, 2)
-    pct_w = (GPA_w/max_GPA)*100
+    pct_w = (raw_GPA_w/max_GPA)*100
     pct_w = round(pct_w, 2)
-    GPA_w = round(GPA_w, 2)
-    GPA_u = round(GPA_u, 2)
-    args = {"GPA_w": GPA_w, "GPA_u": GPA_u, "max_GPA": round(
+    GPA_w = round(raw_GPA_w, 2)
+    GPA_u = round(raw_GPA_u, 2)
+    args = {"GPA_w": GPA_w, "GPA_u": GPA_u, "raw_GPA_w": raw_GPA_w, "raw_GPA_u": raw_GPA_u, "max_GPA": round(
         max_GPA, 2), "pct_u": pct_u, "pct_w": pct_w}
     return HttpResponse(json.dumps(args))
