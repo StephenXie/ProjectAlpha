0716a724daab9f5b82afed972af48723cde00ee9
StephenXie <49465499+StephenXie@users.noreply.github.com>
2022-05-15 13:33:35 -0700

    JournalAdmin q Please enter the commit message for your changes. Lines starting

--- journals/admin.py
+++ journals/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import JournalItem
+
 
 # Register your models here.
+class JournalAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+
+
+admin.site.register(JournalItem, JournalAdmin)
