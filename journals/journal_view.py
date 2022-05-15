e79661b8e7b23b96bfd47b4ea242df7b9b5d1894
StephenXie <49465499+StephenXie@users.noreply.github.com>
2022-05-15 13:21:26 -0700

    update api view to restrict to user

--- journals/journal_view.py
+++ journals/journal_view.py
@@ -5,5 +5,8 @@ from .models import JournalItem
 from rest_framework.permissions import IsAuthenticated
 class JournalView(viewsets.ModelViewSet):  
     permission_classes = (IsAuthenticated,)
-    serializer_class = JournalSerializer   
-    queryset = JournalItem.objects.all()  
+    serializer_class = JournalSerializer    
+    def get_queryset(self):
+        return self.request.user.journals.all()
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
--- journals/models.py
+++ journals/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 from datetime import datetime
 import pytz
 # Create your models here.
 class JournalItem(models.Model):
     id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, related_name="journals", on_delete=models.CASCADE, null=True)
     title = models.TextField()
     date = models.DateTimeField(blank=True, default=datetime.utcnow)
     content = models.TextField(blank=True)
