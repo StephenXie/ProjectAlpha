2d15ce2979c1168338f6e825f4fc36da1829fb45
StephenXie <49465499+StephenXie@users.noreply.github.com>
2022-05-16 12:31:06 -0700

    update settings

--- ProjectX/settings.py
+++ ProjectX/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv(
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'sx-my-app.herokuapp.com',
-                 'www.stephenxie.com', 'www.stephenx.live', 'www.stephenx.tech']
+                 'www.stephenxie.com', 'www.stephenx.live', 'www.stephenx.tech', 'dev.stephenxie.com']
 
 
 # Application definition
